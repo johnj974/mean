a9e05ef43e4cb156330d38fe01d16675ca4955c1
johnj974 <jjdug@hotmail.com>
2021-07-13 17:30:36 +0100

    file input added to form/reactive form implemented

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -17,6 +17,7 @@ export class PostCreateComponent implements OnInit {
   postId: string;
   post: Post;
   isLoading = false;
+  form: FormGroup;
 
   constructor(
     private postsService: PostsService,
@@ -24,6 +25,14 @@ export class PostCreateComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      content: new FormControl(null, Validators.required),
+      image: new FormControl(null, Validators.required),
+    });
+
     this.route.paramMap.subscribe((param: ParamMap) => {
       if (param.has('postId')) {
         this.mode = 'edit';
@@ -36,6 +45,10 @@ export class PostCreateComponent implements OnInit {
             title: postData.title,
             content: postData.content,
           };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content,
+          });
         });
       } else {
         this.mode = 'create';
@@ -44,25 +57,31 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
       this.postsService.updatePost(
         this.postId,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
 
-    form.resetForm();
+    this.form.reset();
   }
 
   getErrorMessage() {
     return 'Please enter valid information';
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
   }
 }
